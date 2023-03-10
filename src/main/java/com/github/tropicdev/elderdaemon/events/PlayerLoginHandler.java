f2fa6d6ba337b4ed72125018df70effc78ec0591
tropicdev <jordan.bmp.mckoy@gmail.com>
2023-03-10 14:18:11 -0500

    Fixed player handling messages

--- src/main/java/com/github/tropicdev/elderdaemon/events/PlayerLoginHandler.java
+++ src/main/java/com/github/tropicdev/elderdaemon/events/PlayerLoginHandler.java
@@ -1,7 +1,6 @@
 package com.github.tropicdev.elderdaemon.events;
 
 import com.github.tropicdev.elderdaemon.Daemon;
-import com.github.tropicdev.elderdaemon.config.Config;
 import com.github.tropicdev.elderdaemon.connections.SocketClient;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.MinecraftServer;
@@ -19,10 +18,12 @@ public class PlayerLoginHandler implements ServerPlayConnectionEvents.Join {
 
             socketClient.emitJoinEvent(handler.getPlayer().getUuidAsString());
 
-            Daemon.LOGGER.info(handler.getPlayer().getUuidAsString() + " Has joined the server");
+            Daemon.LOGGER.info(handler.getPlayer().getName() + " has joined the server, starting session");
 
         } catch (Error e) {
+
             Daemon.LOGGER.error(e.getMessage());
+
         }
 
     }
--- src/main/java/com/github/tropicdev/elderdaemon/events/PlayerLogoutHandler.java
+++ src/main/java/com/github/tropicdev/elderdaemon/events/PlayerLogoutHandler.java
@@ -1,7 +1,6 @@
 package com.github.tropicdev.elderdaemon.events;
 
 import com.github.tropicdev.elderdaemon.Daemon;
-import com.github.tropicdev.elderdaemon.config.Config;
 import com.github.tropicdev.elderdaemon.connections.SocketClient;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
@@ -18,12 +17,11 @@ public class PlayerLogoutHandler implements ServerPlayConnectionEvents.Disconnec
 
             socketClient.emitLeaveEvent(handler.getPlayer().getUuidAsString());
 
-            Daemon.LOGGER.info(handler.getPlayer().getUuidAsString() + " Has joined the server");
+            Daemon.LOGGER.info(handler.getPlayer().getName() + " has left the server, ending session");
 
         } catch (Error e) {
             Daemon.LOGGER.error(e.getMessage());
         }
 
-        Daemon.LOGGER.info(handler.getPlayer().getUuidAsString() + " Has left the server");
     }
 }
